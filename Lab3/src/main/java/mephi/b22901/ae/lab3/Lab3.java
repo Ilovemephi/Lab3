1ae9decda88d13d5e99a2e00225c0ce5f5201bc3
Ilovemephi <egorkin.2004@mail.ru>
2025-04-15 16:18:46 +0300

    commit1
    Создал класс монстр

---
+++ Lab3/src/main/java/mephi/b22901/ae/lab3/Lab3.java
@@ -0,0 +1,10 @@
+
+package mephi.b22901.ae.lab3;
+
+
+public class Lab3 {
+
+    public static void main(String[] args) {
+ 
+    }
+}
---
+++ Lab3/src/main/java/mephi/b22901/ae/lab3/Monster.java
@@ -0,0 +1,133 @@
+
+package mephi.b22901.ae.lab3;
+
+
+public class Monster {
+    
+    private String infoType;
+    private String name;
+    private String info;
+    private int danger;
+    private String residence;
+    private String firstMention;
+    private String vulnerability;
+    private int height;
+    private int weight;
+    private String immunity;
+    private String activityTime;
+    private String recipe;
+    private int time;
+    private String efficiency;
+    
+    public void setInfoType(String infoType) {
+        this.infoType = infoType;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public void setDanger(int danger) {
+        this.danger = danger;
+    }
+
+    public void setResidence(String residence) {
+        this.residence = residence;
+    }
+
+    public void setFirstMention(String firstMention) {
+        this.firstMention = firstMention;
+    }
+    
+    public void setVulnerability(String vulnerability) {
+        this.vulnerability = vulnerability;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setImmunity(String immunity) {
+        this.immunity = immunity;
+    }
+
+    public void setActivityTime(String activityTime) {
+        this.activityTime = activityTime;
+    }
+
+    public void setRecipe(String recipe) {
+        this.recipe = recipe;
+    }
+    
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public void setEfficiency(String efficiency) {
+        this.efficiency = efficiency;
+    }
+    
+    public String getInfoType() { 
+        return infoType; 
+    }
+    
+    public String getName() { 
+        return name; 
+    }
+    public String getInfo() { 
+        return info; 
+    }
+    
+    public int getDanger() { 
+        return danger; 
+    }
+    
+    public String getResidence() { 
+        return residence; 
+    }
+    
+    public String getFirstMention() { 
+        return firstMention; 
+    }
+    
+    public String getVulnerability() { 
+        return vulnerability; 
+    }
+    
+    public int getHeight() { 
+        return height; 
+    }
+    
+    public int getWeight() { 
+        return weight; 
+    }
+    
+    public String getImmunity() { 
+        return immunity; 
+    }
+    
+    public String getActivityTime() { 
+        return activityTime; 
+    }
+    
+    public String getRecipe() { 
+        return recipe; 
+    }
+    
+    public int getTime() { 
+        return time; 
+    }
+    
+    public String getEfficiency() { 
+        return efficiency; 
+    }
+       
+}
